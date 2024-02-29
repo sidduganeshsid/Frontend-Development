b3b3c149ef7c0ece459c7efed79992f565836a3d
sidduganeshsid <sidduganeshengineer@gmail.com>
2024-03-01 04:15:36 +0530

    UPDATE

--- React JS/PRACTICE/ManualCounter/src/App.jsx
+++ React JS/PRACTICE/ManualCounter/src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(1);
+
+  // let count = 15;
+
+  const addValue = ()=>{
+    if(count < 100)
+    setCount(count += 1);
+    console.log(count);
+  }
+
+  const removeValue = ()=>{
+    if(count > 0)
+    setCount(count -=1);
+    console.log(count);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Manual Counter Upto 100</h1>
+     <h2>Counter : {count}</h2>
+     <button
+     onClick={addValue}
+     >Add Value by 1</button>
+     <button
+     onClick={removeValue}
+     >Remove Value by 1</button>
     </>
   )
 }
